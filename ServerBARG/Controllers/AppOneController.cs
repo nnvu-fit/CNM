71e88899a56ad0fb8a87116a14694059b4358d21
Bùi Hoài Phong <1412676@student.hcmus.edu.vn>
2017-11-13 23:29:08 +0700

    server put call

---
+++ ServerBARG/Controllers/AppOneController.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using ServerBARG.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace ServerBARG.Controllers
+{
+    public class AppOneController : ApiController
+    {
+        [HttpPut]
+        [Route("api/managerapp1/putcall")]
+        public HttpResponseMessage GetMoney([FromBody]Call call)
+        {
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(
+                new
+                {
+                    address = call.Address,
+                    addressFormated = call.AddressFormated,
+                    status = call.Status,
+                    time = call.Time,
+                    typeCar = call.TypeCar
+                });
+            var request = WebRequest.CreateHttp("https://barg-9f201.firebaseio.com/" + call.Phone + "/.json");
+            request.Method = "PUT";
+            request.ContentType = "application/json";
+            var buffer = Encoding.UTF8.GetBytes(json);
+            request.ContentLength = buffer.Length;
+            request.GetRequestStream().Write(buffer, 0, buffer.Length);
+            var response = request.GetResponse();
+            return Request.CreateResponse(response);
+        }
+    }
+}
---
+++ ServerBARG/Models/call.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServerBARG.Models
+{
+    public class Call
+    {
+        string phone;
+        string address;
+        string addressFormated;
+        string status;
+        int time;
+        string typeCar;
+
+        public string Phone
+        {
+            get
+            {
+                return phone;
+            }
+
+            set
+            {
+                phone = value;
+            }
+        }
+
+        public string Address
+        {
+            get
+            {
+                return address;
+            }
+
+            set
+            {
+                address = value;
+            }
+        }
+
+        public string AddressFormated
+        {
+            get
+            {
+                return addressFormated;
+            }
+
+            set
+            {
+                addressFormated = value;
+            }
+        }
+
+        public string Status
+        {
+            get
+            {
+                return status;
+            }
+
+            set
+            {
+                status = value;
+            }
+        }
+
+        public string TypeCar
+        {
+            get
+            {
+                return typeCar;
+            }
+
+            set
+            {
+                typeCar = value;
+            }
+        }
+
+        public int Time
+        {
+            get
+            {
+                return time;
+            }
+
+            set
+            {
+                time = value;
+            }
+        }
+    }
+}
