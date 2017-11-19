85d99a94c2e7d25d24069455730ae467dace2ed2
nnvuf <nnvu.fit@hotmail.com>
2017-11-19 19:19:27 +0700

    Add App2

---
+++ coordinates/ClientApp/components/Coordinates.tsx
@@ -0,0 +1,19 @@
+﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
+// for more information see the following page on the TypeScript wiki:
+// https://github.com/Microsoft/TypeScript/wiki/JSX
+
+import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+
+export class Coordinates extends React.Component<RouteComponentProps<{}>, {}> {
+
+    public getMap() {
+
+    }
+
+    public render() {
+        return <div>
+            <div id="map"></div>
+        </div>;
+    }
+}
